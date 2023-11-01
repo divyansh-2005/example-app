689dac040e6af9a53078c865522f8a94409d792b
“divyansh-2005” <divyanshkumbhat@gmail.com>
2023-11-01 23:26:03 +0530

    number swapping code readable and maintainable

---
+++ practicals/number_swapping.php
@@ -0,0 +1,16 @@
+<?php
+// Initialize the variables
+$a = 5;
+$b = 10;
+
+// Display the initial values
+echo "Before swapping: a = $a, b = $b\n";
+
+// Swap the numbers using a temporary variable
+$temp = $a;
+$a = $b;
+$b = $temp;
+
+// Display the swapped values
+echo "After swapping: a = $a, b = $b\n";
+?>
